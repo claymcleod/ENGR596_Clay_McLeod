036f9f7163e2cf6013748e9e81f01fe346b46fc3
Clay McLeod <clay.l.mcleod@gmail.com>
2015-01-27 17:06:53 -0600

    Completed functionality for the SimpleTipCalculator project

---
+++ SimpleTipCalculator/SimpleTipCalculator/MainViewController.swift
@@ -0,0 +1,104 @@
+//
+//  MainViewController.swift
+//  SimpleTipCalculator
+//
+//  Created by Clay McLeod on 1/27/15.
+//  Copyright (c) 2015 Clay McLeod. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipDueLabel: UILabel!
+    @IBOutlet weak var tipStepper: UIStepper!
+    @IBOutlet weak var totalDueLabel: UILabel!
+    @IBOutlet weak var totalTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*
+         *  Dismiss keyboard when user taps on background
+         */
+        
+        var tapGestureRecognizer : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("resignKeyboard"));
+        self.view.addGestureRecognizer(tapGestureRecognizer);
+        
+        /*
+         *  Programmatically change the frame height of the text field
+         */
+        
+        var modifiedFrame = totalTextField.frame;
+        modifiedFrame.size.height = 70;
+        totalTextField.frame = modifiedFrame;
+        
+        /*
+         *  Add the 'Done' button to the toolbar above the keypad.
+         */
+        
+        var toolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.size.width, height: 44.0));
+        
+        toolbar.tintColor = UIColor.whiteColor();
+        toolbar.barStyle = UIBarStyle.Black;
+        toolbar.translucent = true;
+        
+        let barButtonItems = [
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action:nil),
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: Selector("resignKeyboard"))
+        ]
+    
+        toolbar.setItems(barButtonItems, animated: false);
+        
+        totalTextField.inputAccessoryView = toolbar;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func performTipCalculation() {
+        
+        let tipPercentage : Int = Int(tipStepper.value);
+        let total : Double = (totalTextField.text as NSString).doubleValue;
+        let tipAmount : Double = (Double(tipPercentage) / 100) * total;
+        let totalDue : Double = total + tipAmount;
+        
+        if totalTextField.text.isEmpty {
+            
+            tipLabel.text = "Tip Percentage: \(tipPercentage)%";
+            tipDueLabel.text = "Please input a valid total!";
+            totalDueLabel.text = "";
+            
+        } else {
+        
+            var properlyFormattedTipDue : String = String(format: "%0.2f",tipAmount);
+            var properlyFormattedTotalDue : String = String(format: "%0.2f",totalDue);
+        
+            tipLabel.text = "Tip Percentage: \(tipPercentage)%";
+            tipDueLabel.text = "Tip Due: $\(properlyFormattedTipDue)";
+            totalDueLabel.text = "Total Due: $\(properlyFormattedTotalDue)";
+            
+        }
+    }
+    
+    func resignKeyboard() {
+        /*
+         *  Resigns the keyboard from view
+         */
+        
+        totalTextField.resignFirstResponder();
+    }
+    
+    
+    @IBAction func totalValueFinishedEditting(sender: AnyObject) {
+        resignKeyboard();
+        performTipCalculation();
+    }
+    
+    @IBAction func tipValueChanged(sender: AnyObject) {
+        resignKeyboard();
+        performTipCalculation();
+    }
+}
