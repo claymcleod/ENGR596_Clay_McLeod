3aae3a46127555a011fb98c423f37edf9ae26acd
Clay McLeod <clay.l.mcleod@gmail.com>
2015-02-03 23:54:27 -0600

    latest for flickr app

--- FlickrApplication/FlickrApplication/FACollectionViewController.swift
+++ FlickrApplication/FlickrApplication/FACollectionViewController.swift
@@ -8,94 +8,96 @@
 
 import UIKit
 
-let reuseIdentifier = "Cell"
-
-class FACollectionViewController: UICollectionViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        //#warning Incomplete method implementation -- Return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //#warning Incomplete method implementation -- Return the number of items in the section
-        return 0
-    }
-
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+class FACollectionViewController: UICollectionViewController, UITextFieldDelegate, UICollectionViewDelegateFlowLayout {
     
-        // Configure the cell
-    
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
-    
-    }
-    */
+    private let reuseIdentifier = "FlickrCell"
     
     private var searches = [FlickrSearchResults]()
     private let flickr = Flickr()
     
     func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
-        return searches[indexPath.section].searchResults[indexPath.row]
+    return searches[indexPath.section].searchResults[indexPath.row]
     }
-
+    
+    // MARK : UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+    // 1
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+    textField.addSubview(activityIndicator)
+    activityIndicator.frame = textField.bounds
+    activityIndicator.startAnimating()
+    flickr.searchFlickrForTerm(textField.text) {
+    results, error in
+    
+    //2
+    activityIndicator.removeFromSuperview()
+    if error != nil {
+    println("Error searching : \(error)")
+    }
+    
+    if results != nil {
+    //3
+    println("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+    self.searches.insert(results!, atIndex: 0)
+    
+    //4
+    self.collectionView?.reloadData()
+    }
+    }
+    
+    textField.text = nil
+    textField.resignFirstResponder()
+    return true
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    //1
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return searches.count
+    }
+    
+    //2
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    //1
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as FACollectionViewCell
+    //2
+    let flickrPhoto = photoForIndexPath(indexPath)
+    cell.backgroundColor = UIColor.blackColor()
+    //3
+    cell.imageView.image = flickrPhoto.thumbnail
+    
+    return cell
+    }
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    //1
+    func collectionView(collectionView: UICollectionView!,
+    layout collectionViewLayout: UICollectionViewLayout!,
+    sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+    
+    let flickrPhoto =  photoForIndexPath(indexPath)
+    //2
+    if var size = flickrPhoto.thumbnail?.size {
+    size.width += 10
+    size.height += 10
+    return size
+    }
+    return CGSize(width: 100, height: 100)
+    }
+    
+    //3
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    func collectionView(collectionView: UICollectionView!,
+    layout collectionViewLayout: UICollectionViewLayout!,
+    insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+    return sectionInsets
+    }
 }
