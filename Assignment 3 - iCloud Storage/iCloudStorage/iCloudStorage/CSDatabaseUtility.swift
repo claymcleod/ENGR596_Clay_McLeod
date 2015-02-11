0966ae210c3c266cc0d8001af777d31ecb1a88aa
Clay McLeod <clay.l.mcleod@gmail.com>
2015-02-11 15:28:22 -0600

    Finalized getting and receiving keybased queries

---
+++ Assignment 3 - iCloud Storage/iCloudStorage/iCloudStorage/CSDatabaseUtility.swift
@@ -0,0 +1,86 @@
+/*
+
+  CSDatabaseUtility.swift
+  iCloudStorage
+
+  Created by Clay McLeod on 2/11/15.
+  Copyright (c) 2015 Clay McLeod. All rights reserved.
+
+  -----------------------------------------------------
+
+    Types able to be stored in iCloud:
+
+    (1) NSString
+    (2) NSNumber
+    (3) NSData
+    (4) NSDate
+    (5) NSArray
+    (6) CLLocation
+    (7) CKAsset
+    (8) CKReference
+
+*/
+
+import Foundation
+import CloudKit
+
+let _databaseUtility : CSDatabaseUtility = { CSDatabaseUtility() }()
+
+class CSDatabaseUtility: NSObject {
+    
+    class func sharedDatabaseUtility() -> CSDatabaseUtility {
+        return _databaseUtility
+    }
+    
+    var publicDatabase : CKDatabase?
+    var privateDatabase : CKDatabase?
+    
+    private override init () {
+        publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
+        privateDatabase = CKContainer.defaultContainer().privateCloudDatabase
+    }
+    
+    func saveSampleItemsToPublicDatabase() {
+        // Saving records to database
+        
+        let partyRecord : CKRecord = CKRecord(recordType: "Party")
+        partyRecord.setObject("UMiOS Bar Crawl", forKey: "Description")
+        partyRecord.setObject(NSDate(timeIntervalSinceNow: 24 * 3600), forKey: "Date")
+        
+        let barRecord : CKRecord = CKRecord(recordType: "Bar")
+        barRecord.setObject("Corner Bar", forKey: "Name")
+        
+        let partyReference : CKReference = CKReference(record: partyRecord, action: .None)
+        barRecord.setObject(partyReference, forKey: "Party")
+        
+        publicDatabase?.saveRecord(partyRecord, completionHandler: { (record, error) -> Void in
+            if (error != nil) {
+                println("[DatabaseUtility] Error: \(error.localizedDescription)")
+            } else {
+                println("[DatabaseUtility] Saved sample record: partyRecord!")
+            }
+        })
+        
+        
+        publicDatabase?.saveRecord(barRecord, completionHandler: { (record, error) -> Void in
+            if (error != nil) {
+                println("[DatabaseUtility] Error: \(error.localizedDescription)")
+            } else {
+                println("[DatabaseUtility] Saved sample record: barRecord!")
+            }
+        })
+    }
+    
+    func queryPublicDatabaseForName(recordType: NSString, name: NSString) {
+        
+        // Predicate formatting guide: https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/Predicates/Articles/pSyntax.html#//apple_ref/doc/uid/TP40001795
+        
+        let predicate = NSPredicate(format: "Name = %@", name)
+        let query = CKQuery(recordType: recordType, predicate: predicate)
+        
+        publicDatabase?.performQuery(query, inZoneWithID: nil, completionHandler: { (results, error) -> Void in
+            println("[DatabaseUtility] Found \(results.count) results.")
+            println("  |\n   --> Predicate: \(predicate?.description)\n")
+        })
+    }
+}
--- Assignment 3 - iCloud Storage/iCloudStorage/iCloudStorage/ViewController.swift
+++ Assignment 3 - iCloud Storage/iCloudStorage/iCloudStorage/ViewController.swift
@@ -1,75 +1,41 @@
-//
-//  ViewController.swift
-//  iCloudStorage
-//
-//  Created by Clay McLeod on 2/11/15.
-//  Copyright (c) 2015 Clay McLeod. All rights reserved.
-//
+/*
+
+  ViewController.swift
+  iCloudStorage
+
+  Created by Clay McLeod on 2/11/15.
+  Copyright (c) 2015 Clay McLeod. All rights reserved.
+
+*/
 
 import UIKit
-import CloudKit
 
 class ViewController: UIViewController {
 
-    var publicDatabase : CKDatabase?
-    var privateDatabase : CKDatabase?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
-        privateDatabase = CKContainer.defaultContainer().privateCloudDatabase
-        
-        let partyRecord : CKRecord = CKRecord(recordType: "Party")
-        partyRecord.setObject("UMiOS Bar Crawl", forKey: "Description")
-        partyRecord.setObject(NSDate(timeIntervalSinceNow: 24 * 3600), forKey: "Date")
+        let databaseUtility : CSDatabaseUtility = CSDatabaseUtility.sharedDatabaseUtility()
         
         /*
-            Types able to be stored in iCloud:
         
-                (1) NSString
-                (2) NSNumber
-                (3) NSData
-                (4) NSDate
-                (5) NSArray
-                (6) CLLocation
-                (7) CKAsset
-                (8) CKReference
+        // Sample item creation
+        databaseUtility.saveSampleItemsToPublicDatabase()
+        
         */
         
-        let barRecord : CKRecord = CKRecord(recordType: "Bar")
-        barRecord.setObject("Corner Bar", forKey: "Name")
         
-        let partyReference : CKReference = CKReference(record: partyRecord, action: .None)
-        barRecord.setObject(partyReference, forKey: "Party")
 
-        /*
-
-        // Saving records to database
-        
-        publicDatabase?.saveRecord(partyRecord, completionHandler: { (record, error) -> Void in
-            if (error != nil) {
-                println("Error: \(error.localizedDescription)")
-            } else {
-                println("Success for partyRecord!")
-            }
-        })
-        
-        publicDatabase?.saveRecord(barRecord, completionHandler: { (record, error) -> Void in
-            if (error != nil) {
-                println("Error: \(error.localizedDescription)")
-            } else {
-                println("Success for barRecord!")
-            }
-        })
-
-        */
+        // Querying the database
+        databaseUtility.queryPublicDatabaseForName("Bar", name: "Corner Bar")
+        databaseUtility.queryPublicDatabaseForName("Bar", name: "Funkys")
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
